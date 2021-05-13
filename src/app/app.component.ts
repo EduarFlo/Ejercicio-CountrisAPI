8903268937554aeae139ed3a8b060d627f148c98
Eduardo Gonzalez <eduardo.gonzalez.flores28@gmail.com>
2021-05-13 01:12:35 -0500

    Estilizando el proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './services/api.service';
+import { Theme, ThemeService } from './services/theme.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  theme: Observable<Theme>;
+
+  constructor (private themeService: ThemeService, private api: ApiService){}
+
+  ngOnInit(){
+    this.theme = this.themeService.moode$;
+  }
+
+  
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent, HomeComponent, DetailComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
